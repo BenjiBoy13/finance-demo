f1312f439ff3ad8d8208f23c272e17e1602ae57e
Benjamin <ben99.planetatierra@gmail.com>
2022-04-15 12:49:06 -0500

    First Commit

---
+++ src/main/java/com/benjamin/financedemo/FinanceDemoApplication.java
@@ -0,0 +1,13 @@
+package com.benjamin.financedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinanceDemoApplication.class, args);
+    }
+
+}
